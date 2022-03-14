bc9e73f92bd7d2a85a956f7014e1a3489a4e5c61
JefferyHcool <1063474837@qq.com>
2022-03-15 00:49:49 +0800

    制作了添加单词界面

---
+++ miniprogram/components/wordform/wordform.js
@@ -0,0 +1,27 @@
+// components/wordform/wordform.js
+Component({
+        /**
+         * 组件的属性列表
+         */
+        properties: {
+                show:{
+                        'close':Boolean
+                }
+        },
+
+        /**
+         * 组件的初始数据
+         */
+        data: {
+               
+        },
+
+        /**
+         * 组件的方法列表
+         */
+        methods: {
+                close(){
+                        //  this.triggerEvent
+                }
+        }
+})
